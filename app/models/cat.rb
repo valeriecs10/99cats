038abb8445ec61ea7fbe02af20df9e78e050f339
Valerie Campbell <valeriecs10@gmail.com>
2020-12-30 16:26:47 -0800

    added :rental_requests association

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -10,6 +10,12 @@ class Cat < ApplicationRecord
   validates :birth_date, :color, :name, :sex, :description, presence: true
   validate :sex_format
   validates_inclusion_of :color, :in => COLORS
+
+  has_many :rental_requests,
+    class_name: :CatRentalRequest,
+    foreign_key: :cat_id,
+    primary_key: :id,
+    dependent: :destroy
   
     def age
       obj_age = Date.today.year - birth_date.year 
