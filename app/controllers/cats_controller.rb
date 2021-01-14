77a9f3bf91d3ecec1af2122850eaf0958cc35c42
Valerie Campbell <valeriecs10@gmail.com>
2021-01-14 12:55:30 -0800

    added rental_requests to show method

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,6 +6,7 @@ class CatsController < ApplicationController
 
   def show
     @cat = Cat.find_by_id(params[:id])
+    @rental_requests = CatRentalRequest.where(cat_id: params[:id]).order(:start_date)
     
     if @cat
       render :show
